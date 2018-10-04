fce3c8dbabff1829d5d2dc806d33643a7630c3b8
Raz <razli1993@hotmail.com>
2018-10-04 15:31:39 +0800

    Fragments are not destroyed when changing tabs Readded default toolbar

--- app/src/main/java/com/example/razli/newit/MainActivity.kt
+++ app/src/main/java/com/example/razli/newit/MainActivity.kt
@@ -3,58 +3,52 @@ package com.example.razli.newit
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.widget.Toast
+import android.support.v4.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment.newInstance()
+    private val browseFragment = BrowseFragment.newInstance()
+    private val bookmarksFragment = BookmarksFragment.newInstance()
+    private val fragmentManager = supportFragmentManager
+
+    private var activeFragment: Fragment = homeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.fragment_container, HomeFragment.newInstance(), "HomeFragment")
-                    .commit()
-        }*/
-
-        val homeFragment = HomeFragment.newInstance()
-        val browseFragment = BrowseFragment.newInstance()
-        val bookmarksFragment = BookmarksFragment.newInstance()
+        // Add 3 fragments but hide 2
+        fragmentManager.beginTransaction().add(R.id.fragment_container, bookmarksFragment, "2").hide(bookmarksFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.fragment_container, browseFragment, "1").hide(browseFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.fragment_container, homeFragment, "0").commit()
 
         bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home_tab -> {
-                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-                    setFragment(homeFragment)
+                    fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit();
+                    activeFragment = homeFragment
+                    return@setOnNavigationItemSelectedListener true
                 }
 
                 R.id.browse_tab -> {
-                    Toast.makeText(this, "Browse", Toast.LENGTH_SHORT).show()
-                    setFragment(browseFragment)
+                    fragmentManager.beginTransaction().hide(activeFragment).show(browseFragment).commit();
+                    activeFragment = browseFragment
+                    return@setOnNavigationItemSelectedListener true
                 }
 
                 R.id.bookmark_tab -> {
-                    Toast.makeText(this, "Bookmark", Toast.LENGTH_SHORT).show()
-                    setFragment(bookmarksFragment)
+                    fragmentManager.beginTransaction().hide(activeFragment).show(bookmarksFragment).commit();
+                    activeFragment = bookmarksFragment
+                    return@setOnNavigationItemSelectedListener true
                 }
 
                 else -> {
                     // Do nothing
                 }
             }
-            return@setOnNavigationItemSelectedListener true
+            return@setOnNavigationItemSelectedListener false
         }
-    }
-
-    private fun setFragment(fragment: Fragment) {
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-
-        fragmentTransaction
-                .replace(R.id.fragment_container, fragment)
-                .commit()
-
     }
 }
